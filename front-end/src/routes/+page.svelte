d735d9f9a1ec6accff72ffc988c0aa33aa8c44fd
Michael McLean <michael.i.mclean@gmail.com>
2025-06-11 23:30:44 +1000

    Add SvelteKit frontend scaffold

--- front-end/src/routes/+page.svelte
+++ front-end/src/routes/+page.svelte
@@ -1,24 +1,2 @@
-<script>
-  let decks = [];
-  let binders = [];
-
-  // Sample static data; eventually fetch from backend
-  decks = [{ name: "Burn", cards: 60 }];
-  binders = [{ name: "Trade Binder", cards: 120 }];
-</script>
-
-<h1>MTG Manager</h1>
-
-<h2>Decks</h2>
-<ul>
-  {#each decks as deck}
-    <li>{deck.name} ({deck.cards} cards)</li>
-  {/each}
-</ul>
-
-<h2>Binders</h2>
-<ul>
-  {#each binders as binder}
-    <li>{binder.name} ({binder.cards} cards)</li>
-  {/each}
-</ul>
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
